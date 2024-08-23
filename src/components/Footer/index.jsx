53896e0c94e452e6b6f8b7180da572d9a17db6cb
Rafael Altarriba <rafael_altarriba@estudante.sc.senai.br>
2024-08-22 21:34:34 -0300

    projeto react 5

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export function Footer() {
+  return (
+      <footer>
+        <div class="linha"></div>
+        <a href="instagram.com" class="coloridoazul" id="insta">
+          Instagram
+        </a>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="whatsapp.com" class="coloridoazul">
+          Fale Conosco
+        </a>
+      </footer>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,36 @@
+import enderecoImg from '../../../public/img/image.png';
+import { Footer } from '../../components/Footer';
+import './styles.css';
+
+
 export function Home() {
-  return <h1>Pagina principal</h1>;
+
+
+  return (
+    <>
+      <img src={ enderecoImg } alt="Descrição da img"/>
+
+
+      <h1><span class="sombra">Transformando </span><span class="coloridoazul">espaço</span>,
+      <br/> criando <span class="coloridoazul">história</span> </h1>
+
+
+      <p>
+        Nós criamos ambientes  <b> exclusívos </b> e <b> únicos </b>, com muito bom gosto e profissionalismo, <br/> investimos tempo e
+          <span class="coloridoazul"> dedicação no seu projeto</span>. Nosso compromisso vai além de simplesmente decorar.
+      </p>
+
+
+      <br/>
+      <br/>
+      <p>
+          Tenha <span class="coloridoazul">ambientes limpos</span> e bem decorados, transmitindo <b>elegância</b> e <b> finesse </b>para todo <br/> aquele que estiver ali. Sua história visual começa aqui!
+      </p>
+     
+      <Footer />
+
+
+    </>
+   
+  )
 }
